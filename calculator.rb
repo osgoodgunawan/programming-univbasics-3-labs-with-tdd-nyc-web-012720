cb635224686168638920c8e47cd35aff075b4de4
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-04 19:32:59 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-first_number=10
-second_number=24
+first_number=24
+second_number=2
 sum=first_number+second_number
 difference=first_number-second_number
 product=first_number*second_number
